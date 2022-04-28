cfd8ba5b1deb85e9ec1b4354e4e4bd3875521ac7
pinto0309 <rmsdh122@yahoo.co.jp>
2022-04-28 20:34:30 +0900

    Acosh

---
+++ components_of_onnx/ops/Acosh/gen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+OP=Acosh
+LOWEROP=${OP,,}
+
+OPSET=9
+sog4onnx \
+--op_type ${OP} \
+--opset ${OPSET} \
+--op_name ${LOWEROP}${OPSET} \
+--input_variables ${LOWEROP}${OPSET}_input float32 [1] \
+--output_variables ${LOWEROP}${OPSET}_output float32 [1] \
+--output_onnx_file_path ${OP}${OPSET}.onnx \
+--non_verbose
